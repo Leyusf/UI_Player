603dde0113e03515a363031f9bd68e5139bc5b2b
Leyusf <53003567+Leyusf@users.noreply.github.com>
2020-12-16 13:13:18 +0800

    3rd

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include "player.h"
+
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Player w;
+    if (argc==2)
+        w.setPath(argv[1]);
+    w.show();
+    return a.exec();
+}
